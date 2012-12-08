d0dc9217cd8c0a3a0e941314e5833c5c539e955c
buruzaemon <joker@io>
2012-12-08 20:54:50 +0900

    Completed tables and barplots for Ex. 4-1

--- Section_4_Bivariate_Data/section4.R
+++ Section_4_Bivariate_Data/section4.R
@@ -1,5 +1,8 @@
 ## Q 4.1
 #  1. create separate tables for Q1 and Q2 
+#  ... copying in directly from text, so
+#      we will need to delete the leading column
+#      for Students ...
 tmp <- read.table(textConnection("1 3 5 1
 2 3 2 3
 3 3 5 1
@@ -10,46 +13,66 @@ tmp <- read.table(textConnection("1 3 5 1
 8 4 5 1
 9 3 4 1
 10 4 2 1"))
-tmp <- tmp[-1]
-table.Q1 <- tmp[1]
-colnames(table.Q1) <- c("Ques. 1")
-table.Q1
-table.Q2 <- tmp[2]
-colnames(table.Q2) <- c("Ques. 2")
-table.Q2
+#  ... transpose and add rownames, 
+#      to make it easier to read ...
+tmp <- t(tmp[-1])
+rownames(tmp) <- c('Q1', 'Q2', 'Q3')
+
+#  table for Q1
+q1 <- factor(rep('Q1', 10))
+r1 <- factor(tmp[1,], levels=c(1,2,3,4,5))
+table.q1 <- table(q1, r1)
+png(filename='images/4-1-1a.png')
+barplot(table.q1, 
+        main="Rankings for Q1",
+	xlab="Rankings",
+        col='darkblue', 
+	legend.text=T)
+dev.off()
+
+#  table for Q2
+q2 <- factor(rep('Q2', 10))
+r2 <- factor(tmp[2,], levels=c(1,2,3,4,5))
+table.q2 <- table(q2, r2)
+png(filename='images/4-1-1b.png')
+barplot(table.q2, 
+        main="Rankings for Q2",
+	xlab="Rankings",
+        col='red', 
+	legend.text=T)
+dev.off()
 
 #  2. create a contigency table
-table.contingency <- data.frame()
-for (c in c(1:ncol(tmp)))
-{
-    count <- c(NA, NA, NA, NA, NA)
-    for (i in (1:5))
-    {
-      count[i] <- sum(tmp[,c]==i)
-    }
-    table.contingency <- rbind(table.contingency, count)
-}
+q1_2 <- factor(rep(c('Q1', 'Q2'), each=10))
+r1_2 <- factor(append(tmp[1,], tmp[2,]), levels=c(1,2,3,4,5))
+table(q1_2, r1_2)
 
-total.cols <- c(NA, NA, NA, NA, NA)
-for (c in c(1:ncol(table.contingency)))
-{
-  total.cols[c] <- sum(table.contingency[,c])
-}
-table.contingency <- rbind(table.contingency, total.cols)
+#  3. stacked table of Q2 and Q3
+png(filename='images/4-1-3.png')
+q2_3 <- factor(rep(c('Q2', 'Q3'), each=10))
+r2_3 <- factor(append(tmp[2,], tmp[3,]), levels=c(1,2,3,4,5))
+table.q2_3 = table(q2_3, r2_3)
+barplot(table.q2_3, 
+        main="Q2 vs Q3",
+	xlab="Rankings",
+	col=c('red', 'darkgreen'),
+	legend=T)
+dev.off()
 
-total.rows <- c(NA, NA, NA, NA)
-for (r in c(1:nrow(table.contingency)))
-{
-  total.rows[r] <- sum(table.contingency[r,])
-}
-table.contingency <- cbind(table.contingency, total.rows)
+#  4. all Q's, side-by-side
+png(filename='images/4-1-4.png')
+q.all <- factor(rep(c('Q1', 'Q2', 'Q3'), each=10))
+r.all <- factor(append(append(tmp[1,], tmp[2,]), tmp[3,]), levels=c(1,2,3,4,5))
+table.all <- table(q.all, r.all)
+barplot(table.all, 
+        main="All Q's, Side-by-side",
+	xlab="Rankings",
+	col=c('darkblue', 'red', 'darkgreen'),
+	legend=T,
+	beside=T)
+dev.off()
 
-colnames(table.contingency) <- c('1', '2', '3', '4', '5', 'Totals')
-rownames(table.contingency) <- c('Q1', 'Q2', 'Q3', 'Totals')
-table.contingency
-
-
-
+#4
 
 
 ## Q 4
