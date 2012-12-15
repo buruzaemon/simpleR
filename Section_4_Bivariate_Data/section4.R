cdf1c7f786b3cf692383055509c84286a77c4dc2
buruzaemon <joker@io>
2012-12-15 10:13:14 +0900

    Finished w/ 4.9

--- Section_4_Bivariate_Data/section4.R
+++ Section_4_Bivariate_Data/section4.R
@@ -630,10 +630,82 @@ summary(women)
 
 png(filename='images/4-8.png')
 plot(height, weight,
-     main="4.7 - Women: Height vs Weight",
+     main="4.8 - Women: Height vs Weight",
      xlab="Height",
      ylab="Weight")
 abline(lm(weight ~ height), col="red")
 dev.off()
 
 detach(women)
+
+
+## Q 4.9
+attach(mtcars)
+summary(mtcars)
+#      mpg             cyl             disp             hp       
+# Min.   :10.40   Min.   :4.000   Min.   : 71.1   Min.   : 52.0  
+# 1st Qu.:15.43   1st Qu.:4.000   1st Qu.:120.8   1st Qu.: 96.5  
+# Median :19.20   Median :6.000   Median :196.3   Median :123.0  
+# Mean   :20.09   Mean   :6.188   Mean   :230.7   Mean   :146.7  
+# 3rd Qu.:22.80   3rd Qu.:8.000   3rd Qu.:326.0   3rd Qu.:180.0  
+# Max.   :33.90   Max.   :8.000   Max.   :472.0   Max.   :335.0  
+#     drat             wt             qsec             vs        
+# Min.   :2.760   Min.   :1.513   Min.   :14.50   Min.   :0.0000  
+# 1st Qu.:3.080   1st Qu.:2.581   1st Qu.:16.89   1st Qu.:0.0000  
+# Median :3.695   Median :3.325   Median :17.71   Median :0.0000  
+# Mean   :3.597   Mean   :3.217   Mean   :17.85   Mean   :0.4375  
+# 3rd Qu.:3.920   3rd Qu.:3.610   3rd Qu.:18.90   3rd Qu.:1.0000  
+# Max.   :4.930   Max.   :5.424   Max.   :22.90   Max.   :1.0000  
+#       am              gear            carb      
+# Min.   :0.0000   Min.   :3.000   Min.   :1.000  
+# 1st Qu.:0.0000   1st Qu.:3.000   1st Qu.:2.000  
+# Median :0.0000   Median :4.000   Median :2.000  
+# Mean   :0.4062   Mean   :3.688   Mean   :2.812  
+# 3rd Qu.:1.0000   3rd Qu.:4.000   3rd Qu.:4.000  
+# Max.   :1.0000   Max.   :5.000   Max.   :8.000  
+
+#  1. names?
+names(mtcars)
+# [1] "mpg"  "cyl"  "disp" "hp"   "drat" "wt"   "qsec" "vs"   "am"   "gear"
+# [11] "carb"
+
+#  2. max mpg?
+max(mtcars$mpg)
+# [1] 33.9
+
+#  3. and which car is it?
+mtcars[mpg==max(mpg),]
+#                 mpg cyl disp hp drat    wt qsec vs am gear carb
+# Toyota Corolla 33.9   4 71.1 65 4.22 1.835 19.9  1  1    4    1
+
+#  4. list first 5 cars 
+mtcars[1:5,]
+#                    mpg cyl disp  hp drat    wt  qsec vs am gear carb
+# Mazda RX4         21.0   6  160 110 3.90 2.620 16.46  0  1    4    4
+# Mazda RX4 Wag     21.0   6  160 110 3.90 2.875 17.02  0  1    4    4
+# Datsun 710        22.8   4  108  93 3.85 2.320 18.61  1  1    4    1
+# Hornet 4 Drive    21.4   6  258 110 3.08 3.215 19.44  1  0    3    1
+# Hornet Sportabout 18.7   8  360 175 3.15 3.440 17.02  0  0    3    2
+
+#  5. Valiant's hp?
+mtcars['Valiant',]$hp
+# [1] 105
+
+#  6. Merc 450LC?
+mtcars['Merc 450SLC',]
+#              mpg cyl  disp  hp drat   wt qsec vs am gear carb
+# Merc 450SLC 15.2   8 275.8 180 3.07 3.78   18  0  0    3    3
+
+#  7. cyl vs mpg?
+png(filename='images/4-9.png')
+plot(cyl, mpg,
+     main="4.9 - Cars: cyl vs mpg",
+     xlab="Cylinders",
+     ylab="MPG")
+abline(lm(mpg ~ cyl), col="red")
+dev.off()
+
+detach(mtcars)
+
+
+
