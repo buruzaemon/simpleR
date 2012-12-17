3c2016e02c6942c78f848f61b6fc709c909cd46c
buruzaemon <joker@io>
2012-12-17 23:01:24 +0900

    Ex. 5.2

--- Section_5_Multivariate_Data/section5.R
+++ Section_5_Multivariate_Data/section5.R
@@ -23,3 +23,30 @@ boxplot(emissions[-1,],
 dev.off()
 
 
+## Q 5.2
+data(package="UsingR", chips)
+summary(chips)
+#     wafer11          wafer12        wafer13        wafer14      
+#  Min.   : 840.0   Min.   : 850   Min.   : 830   Min.   : 850.0  
+#  1st Qu.: 982.5   1st Qu.: 960   1st Qu.: 970   1st Qu.: 972.5  
+#  Median :1030.0   Median :1020   Median :1020   Median :1005.0  
+#  Mean   :1014.7   Mean   :1009   Mean   :1014   Mean   :1010.7  
+#  3rd Qu.:1050.0   3rd Qu.:1060   3rd Qu.:1060   3rd Qu.:1060.0  
+#  Max.   :1120.0   Max.   :1120   Max.   :1120   Max.   :1120.0  
+#     wafer21          wafer22          wafer23        wafer24    
+#  Min.   : 840.0   Min.   : 900.0   Min.   : 910   Min.   : 900  
+#  1st Qu.: 992.5   1st Qu.: 982.5   1st Qu.: 980   1st Qu.: 990  
+#  Median :1020.0   Median :1025.0   Median :1015   Median :1020  
+#  Mean   :1016.0   Mean   :1024.7   Mean   :1020   Mean   :1021  
+#  3rd Qu.:1060.0   3rd Qu.:1070.0   3rd Qu.:1068   3rd Qu.:1060  
+#  Max.   :1120.0   Max.   :1130.0   Max.   :1110   Max.   :1140 
+
+png(filename="images/5-2-1.png")
+boxplot(chips, main="5.2 - chips")
+dev.off()
+
+library(UsingR)
+png(filename="images/5-2-2.png")
+simple.densityplot(chips)
+dev.off()
+# slight variations on mean, but they all seem to be similar
