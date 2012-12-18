e54f4544a71d353c845f19c4fe8f40e3b284bbd5
buruzaemon <buruzaemon@github>
2012-12-18 15:00:13 +0900

    Completed work on Section 5 exercises

--- Section_5_Multivariate_Data/section5.R
+++ Section_5_Multivariate_Data/section5.R
@@ -113,3 +113,81 @@ detach(carbon)
 #  Site 2 seems to have clearly higher levels than the
 #  other 2, which are both at the same level
 
+
+## Q 5.6
+data(package='UsingR', babies)
+summary(babies)
+#       id          pluralty    outcome       date        gestation          sex   
+# Min.   :  15   Min.   :5   Min.   :1   Min.   :1350   Min.   :148.0   Min.   :1  
+# 1st Qu.:5286   1st Qu.:5   1st Qu.:1   1st Qu.:1444   1st Qu.:272.0   1st Qu.:1  
+# Median :6730   Median :5   Median :1   Median :1540   Median :280.0   Median :1  
+# Mean   :6001   Mean   :5   Mean   :1   Mean   :1536   Mean   :286.9   Mean   :1  
+# 3rd Qu.:7583   3rd Qu.:5   3rd Qu.:1   3rd Qu.:1627   3rd Qu.:288.0   3rd Qu.:1  
+# Max.   :9263   Max.   :5   Max.   :1   Max.   :1714   Max.   :999.0   Max.   :1  
+#       wt            parity            race             age              ed       
+# Min.   : 55.0   Min.   : 0.000   Min.   : 0.000   Min.   :15.00   Min.   :0.000  
+# 1st Qu.:108.8   1st Qu.: 0.000   1st Qu.: 0.000   1st Qu.:23.00   1st Qu.:2.000  
+# Median :120.0   Median : 1.000   Median : 3.000   Median :26.00   Median :2.000  
+# Mean   :119.6   Mean   : 1.932   Mean   : 3.206   Mean   :27.37   Mean   :2.922  
+# 3rd Qu.:131.0   3rd Qu.: 3.000   3rd Qu.: 7.000   3rd Qu.:31.00   3rd Qu.:4.000  
+# Max.   :176.0   Max.   :13.000   Max.   :99.000   Max.   :99.00   Max.   :9.000  
+#       ht             wt1          drace             dage            ded       
+# Min.   :53.00   Min.   : 87   Min.   : 0.000   Min.   :18.00   Min.   :0.000  
+# 1st Qu.:62.00   1st Qu.:115   1st Qu.: 0.000   1st Qu.:25.00   1st Qu.:2.000  
+# Median :64.00   Median :126   Median : 3.000   Median :29.00   Median :4.000  
+# Mean   :64.67   Mean   :154   Mean   : 3.665   Mean   :30.74   Mean   :3.189  
+# 3rd Qu.:66.00   3rd Qu.:140   3rd Qu.: 7.000   3rd Qu.:35.00   3rd Qu.:5.000  
+# Max.   :99.00   Max.   :999   Max.   :99.000   Max.   :99.00   Max.   :9.000  
+#      dht             dwt           marital           inc            smoke       
+# Min.   :60.00   Min.   :110.0   Min.   :0.000   Min.   : 0.00   Min.   :0.0000  
+# 1st Qu.:70.00   1st Qu.:165.0   1st Qu.:1.000   1st Qu.: 2.00   1st Qu.:0.0000  
+# Median :73.00   Median :190.0   Median :1.000   Median : 4.00   Median :1.0000  
+# Mean   :81.67   Mean   :505.4   Mean   :1.038   Mean   :13.16   Mean   :0.8681  
+# 3rd Qu.:99.00   3rd Qu.:999.0   3rd Qu.:1.000   3rd Qu.: 7.00   3rd Qu.:1.0000  
+# Max.   :99.00   Max.   :999.0   Max.   :5.000   Max.   :98.00   Max.   :9.0000  
+#     time            number      
+# Min.   : 0.000   Min.   : 0.000  
+# 1st Qu.: 0.000   1st Qu.: 0.000  
+# Median : 1.000   Median : 1.000  
+# Mean   : 1.748   Mean   : 2.604  
+# 3rd Qu.: 1.000   3rd Qu.: 3.000  
+# Max.   :99.000   Max.   :98.000 
+
+attach(babies)
+png(filename='images/5-6-1.png', width=960, height=960)
+pairs(babies)
+dev.off()
+
+#  ... now, looking at this pair-wise comparison, 
+#      which seem to have a linear relation?
+png(filename='images/5-6-2a.png')
+plot(parity, age, 
+    main="5.6 - babies: Linear Relations?")
+abline(lm(age ~ parity), col='red')
+dev.off()
+
+png(filename='images/5-6-2b.png')
+plot(age, dage, 
+    main="5.6 - babies: Linear Relations?")
+abline(lm(dage ~ age), col='red')
+dev.off()
+
+#  ... make a scatter plot of birthweight and gestation,
+#      using different pch for the level of 'factor' smoke
+#      (smoke is not really a factor, but numeric!!!)...
+plot(wt ~ gestation, 
+    main="5.6 - babies: gestation vs weight")
+rect(par("usr")[1],par("usr")[3],par("usr")[2],par("usr")[4],col = "gray")
+tmp = levels(factor(smoke))
+points(wt ~ gestation, 
+    pch=smoke, 
+    col=heat.colors(n=length(tmp)))
+legend(locator(1), 
+    title="Factor: smoke",
+    cex=0.8,
+    legend=tmp, 
+    pch=smoke, 
+    col=heat.colors(n=length(tmp)),
+    horiz=T)
+dev.copy(png, "images/5-6-2c.png")
+dev.off()
