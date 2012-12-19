966623f13a79afc6d0abb78df6242df40611a49c
buruzaemon <buruzaemon@github>
2012-12-19 17:05:48 +0900

    Starting on Section 7, Simulations

---
+++ Section_7_Simulations/section7.R
@@ -0,0 +1,13 @@
+##
+# code for something like Figure 31
+# n = 10
+# p = .25
+# hist(rbinom(100,n,p), breaks=seq(0,6,1), col=gray(.9), prob=T)
+# lines(seq(0,6,1), dbinom(seq(0,6,1), n, p), col='green')
+# dev.copy(png, "images/7-fig-31.png")
+# dev.off()
+
+
+
+
+## Q 7.1
