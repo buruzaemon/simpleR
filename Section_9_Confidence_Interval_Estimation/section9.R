24e76dccffe4e090da84fe617f209804a7019e7e
buruzaemon <buruzaemon@github>
2013-01-07 18:34:35 +0900

    Continuing to work on section 9 exercises

--- Section_9_Confidence_Interval_Estimation/section9.R
+++ Section_9_Confidence_Interval_Estimation/section9.R
@@ -3,38 +3,31 @@ library(UsingR)
 ## Q 9.1
 #  ... assuming that sigma is known
 #      and the X_i are normally distributed...
-simple.z.test <- function(X, sigma, conf.level=0.95)
-{
-    n = length(X)
-    xbar = mean(X)
-    alpha = 1 - conf.level
-    zstar = qnorm(1 - alpha/2)
-    SE = sigma/sqrt(n)
-    xbar + c(-zstar*SE, zstar*SE)
-}
-
 set.seed(9.1)
 mu = 10
 sd = 5
 
 x <- rnorm(15, mu, sd)
 
+#  ... let's just visually examine what we've got...
+#      looks to be fairly normal, in spite of the
+#      small number of samples (15)
 simple.eda(x)
 dev.copy(png, "images/9-1.png")
 dev.off()
 
-#  ... perhaps we have too few measurements?
+xbar <- mean(x)
+se   <- sd/sqrt(length(x))
 
-simple.z.test(x, sd)
-# [1]  7.559284 12.619890
+z <- (xbar - mu) / se
+z
+#[1] 0.06939382
 
-#  ... 95% confidence interval
-#      should be median +/- 1.96
-#      you can see that we are a bit off!
-mean(x)-7.56
-# [1] 2.529587
-12.62 - mean(x)
-# [1] 2.530413
+#  ... and from a standard normal table,
+#      we see that this measurement is roughly
+#      only 2.8% of the population have sample mean
+#      this close away from the population mean (10)
+#      i.e., this particular 1-sample z test got it right
 
 
 ## Q 9.2
@@ -44,21 +37,165 @@ f <- function() mean(rnorm(15, mean=10, sd=5))
 SE <- 5/sqrt(15)
 xbar <- simple.sim(100, f)
 
-alpha = 0.023;zstar = qnorm(1-alpha/2);sum(abs(xbar-10) < zstar*SE)
-# [1] 94
-alpha = 0.022;zstar = qnorm(1-alpha/2);sum(abs(xbar-10) < zstar*SE)
-# [1] 96
-alpha = 0.0225;zstar = qnorm(1-alpha/2);sum(abs(xbar-10) < zstar*SE)
-# [1] 95
+alpha = 0.1;  zstar = qnorm(1-alpha/2); sum(abs(xbar-10) < zstar*SE)
+# [1] 87
+alpha = 0.05; zstar = qnorm(1-alpha/2); sum(abs(xbar-10) < zstar*SE)
+# [1] 93
+alpha = 0.01; zstar = qnorm(1-alpha/2); sum(abs(xbar-10) < zstar*SE)
+# [1] 98
 
-#  ... it appears that 97.75% is in the 95% confidence interval
+#  ... it appears that 93% of our samples calculated above
+#      are in the 95% confidence interval
 
 
 ## 9.3
-set.seed(9.3)
+t.test(x)
+#
+#        One Sample t-test
+#
+# data:  x 
+# t = 9.2971, df = 14, p-value = 2.286e-07
+# alternative hypothesis: true mean is not equal to 0 
+# 95 percent confidence interval:
+#   7.761966 12.417208 
+# sample estimates:
+# mean of x 
+#  10.08959 
+
+t.test(xbar)
+#
+#        One Sample t-test
+#
+# data:  xbar 
+# t = 71.6056, df = 99, p-value < 2.2e-16
+# alternative hypothesis: true mean is not equal to 0 
+# 95 percent confidence interval:
+#   9.818487 10.378143 
+# sample estimates:
+# mean of x 
+#  10.09831 
+
+#  ... this looks to be good, too
+#      note how when increasing n, the 
+#      ??? 
 
 
+## 9.4
+data(package='UsingR', exec.pay)
 
+wilcox.test(exec.pay, conf.int=T, conf.level=.8)
+#
+#        Wilcoxon signed rank test with continuity correction
+#
+# data:  exec.pay 
+# V = 19306, p-value < 2.2e-16
+# alternative hypothesis: true location is not equal to 0 
+# 80 percent confidence interval:
+#  27.00005 31.49996 
+# sample estimates:
+# (pseudo)median 
+#       29.00002 
+
+wilcox.test(exec.pay, conf.int=T, conf.level=.95)
+#
+#        Wilcoxon signed rank test with continuity correction
+#
+# data:  exec.pay 
+# V = 19306, p-value < 2.2e-16
+# alternative hypothesis: true location is not equal to 0 
+# 95 percent confidence interval:
+#  25.99998 32.99994 
+# sample estimates:
+# (pseudo)median 
+#       29.00002 
+
+
+## 9.5
+data(package='UsingR', rat)
+
+simple.eda(rat)
+dev.copy(png, "images/9-5.png")
+dev.off()
+
+## ... i think this small data set looks normal enough to go
+
+t.test(rat)
+#
+#        One Sample t-test
+#
+# data:  rat 
+# t = 14.1763, df = 19, p-value = 1.48e-11
+# alternative hypothesis: true mean is not equal to 0 
+# 95 percent confidence interval:
+#  96.69997 130.20003 
+# sample estimates:
+# mean of x 
+#    113.45 
+
+#  ... let's compare SE...
+(130.20003 - 96.69997)/4
+# [1] 8.375015
+
+sd(rat) / sqrt(length(rat))
+# [1] 8.002787
+
+#  ... we are at 0.956 % accuracy!
+
+
+## 9.6
+data(package='UsingR', puerto)
+
+simple.eda(puerto)
+dev.copy(png, "images/9-6.png")
+dev.off()
+
+#  ... ok, now this guy's Normal plot suggests
+#      that this is not really normal?!
+simple.eda(puerto)
+dev.copy(png, "images/9-6.png")
+dev.off()
+
+#  ... looks pretty normal, actually!
+
+t.test(puerto)
+#
+#        One Sample t-test
+#
+# data:  puerto 
+# t = 25.8466, df = 49, p-value < 2.2e-16
+# alternative hypothesis: true mean is not equal to 0 
+# 95 percent confidence interval:
+#  255.9244 299.0756 
+# sample estimates:
+# mean of x 
+#     277.5
+
+
+## 9.7
+data(package='UsingR', malpract)
+
+simple.eda(malpract)
+dev.copy(png, "images/9-7.png")
+dev.off()
+
+#  ... for median, we use wilcox.test
+#      if this data is not normally distributed,
+#      the median is preferable to the mean
+#      see how the Normal plot is way out of line?
+#      and how the boxplot is skewed to the higher values?p
+
+wilcox.test(malpract, conf.int=T, conf.level=.9)
+#
+#        Wilcoxon signed rank test
+#
+# data:  malpract 
+# V = 153, p-value = 1.526e-05
+# alternative hypothesis: true location is not equal to 0 
+# 90 percent confidence interval:
+#   385 1325 
+# sample estimates:
+# (pseudo)median 
+#            800 
 
 
 
